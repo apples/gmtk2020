1de068f61a63c48087f336651f5d9aec7b035db4
Jeramy Harrison <dbralir@gmail.com>
2020-07-12 12:31:32 -0500

    Added title screen

--- data/scripts/gui/scene_mainmenu/root.lua
+++ data/scripts/gui/scene_mainmenu/root.lua
@@ -2,27 +2,30 @@ local vdom = require('vdom')
 
 -- start_game: () => ()
 return function(props)
-    return vdom.create_element('widget', { width = '100%', height = '100%' },
+    return vdom.create_element('panel', { width = '100%', height = '100%', texture = 'background/0' },
+    vdom.create_element('panel', { width = '100%', height = '100%', texture = 'background/1' },
+vdom.create_element('panel', { width = '100%', height = '100%', texture = 'background/2' },
+vdom.create_element('panel', { width = '50%', height = '50%', valign='center', halign='center', color = '#000c' },
         vdom.create_element(
             'label',
             {
                 halign = 'center',
-                bottom = 720/2 + 80,
+                bottom = 720/2 + 30 * 3,
                 color = '#fff',
-                text = 'Sample Game',
-                height = 24,
+                text = 'Overplanted!',
+                height = 24 * 3,
             }
         ),
         vdom.create_element(
             'label',
             {
                 halign = 'center',
-                bottom = 720/2 - 20,
+                bottom = 720/2 - 60 * 3,
                 color = '#fff',
-                text = '[NEW GAME]',
-                height = 24,
+                text = '[START GAME]',
+                height = 24 * 2,
                 on_click = props.start_game,
             }
         )
-    )
+    ))))
 end
